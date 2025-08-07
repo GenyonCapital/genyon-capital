1a16569d95ded753ed7baddb2cee05c9c5315c52
GenyonCapital <ceo@genyon.eu>
2025-08-07 11:36:08 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'Arial', textAlign: 'center', marginTop: '100px' }}>
+      <h1>GENYON Capital</h1>
+      <p>Em construção. A revolução está a caminho.</p>
+    </div>
+  )
+}
+
+export default App
